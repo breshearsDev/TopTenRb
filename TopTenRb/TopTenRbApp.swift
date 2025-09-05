d4a64e6354e1e42741c4cdd2ec4885d16aeb6c1d
Rob Breshears <robbreshears@gmail.com>
2025-09-05 10:08:10 -0600

    Initial Commit

---
+++ TopTenRb/TopTenRbApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTenRbApp.swift
+//  TopTenRb
+//
+//  Created by Breshears, Rob - CTC on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TopTenRbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TopTenRbTests/TopTenRbTests.swift
@@ -0,0 +1,17 @@
+//
+//  TopTenRbTests.swift
+//  TopTenRbTests
+//
+//  Created by Breshears, Rob - CTC on 9/5/25.
+//
+
+import Testing
+@testable import TopTenRb
+
+struct TopTenRbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
